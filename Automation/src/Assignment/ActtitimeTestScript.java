4fda92c8519d5c420fa925e3b2556daf13efefe2
DELL <DELL@DESKTOP-IF0AQFE>
2024-01-03 09:41:12 -0800

    first commit

---
+++ Automation/src/Assignment/ActtitimeTestScript.java
@@ -0,0 +1,14 @@
+package Assignment;
+
+import java.util.Random;
+
+public class ActtitimeTestScript {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random r= new Random();
+		int no = r.nextInt(1000)
+
+	}
+
+}
